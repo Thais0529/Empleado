d1c1595946b3943b3de79670bb4fb5775373d146
daniela <luanacervantesemma@gmail.com>
2025-04-25 09:03:22 -0500

    Creacion de objeto empleado

---
+++ Empleado.py
@@ -0,0 +1,23 @@
+class Empleado:
+    '''
+    crear objeto de la clase Empleado
+    '''
+
+    def __init__(self, nombre, sueldo):
+        '''
+        constructor de la clase Empleado
+        :param nombre: nombre del empleado
+        :param sueldo: sueldo del empleado
+        :param eliminado: True si el empleado es eliminado, False si el empleado no es eliminado
+        '''
+        self.nombre = nombre
+        self.sueldo = sueldo
+        self.eliminado = False
+
+    @property
+    def nombre(self):
+        return self.nombre
+    @nombre.setter
+    def nombre(self, nombre):
+        self.nombre = nombre
+        
